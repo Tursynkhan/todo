f2212a21afec9f3d71cc1f5b41b5e972c4d99582
Tursynkhan <tursynkhan001@gmail.com>
2023-05-30 23:37:20 +0600

    updated cmd

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/tursynkhan/todo-app"
 	"github.com/tursynkhan/todo-app/pkg/handler"
+	"github.com/tursynkhan/todo-app/pkg/repository"
+	"github.com/tursynkhan/todo-app/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+
+	if err := intiConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func intiConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
