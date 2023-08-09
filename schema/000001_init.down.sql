c26af35b982c5682f34b6a3ef01ebf2e5846229c
Tursynkhan <tursynkhan001@gmail.com>
2023-08-09 12:16:11 +0600

    update: schema

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,5 +1,9 @@
-DROP TABLE users;
-DROP TABLE todo_lists;
-DROP TABLE users_lists;
-DROP TABLE todo_item;
 DROP TABLE lists_items;
+
+DROP TABLE users_lists;
+
+DROP TABLE todo_lists;
+
+DROP TABLE users;
+
+DROP TABLE todo_item;
